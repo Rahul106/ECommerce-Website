e7613f5d0485e8bab5636b88681585c8879863af
Rahul106 <therahulgupta106>
2024-02-20 18:41:08 +0530

    added readme.md with project description

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,8 +24,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
-.sync()
+.sync({force: true})
 .then(result => {
     //console.log(result);
     app.listen(3000);
