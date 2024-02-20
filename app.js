f2e8c79c07d47a7e083e5053966d6fceb6d5eaca
Rahul106 <therahulgupta106>
2024-02-21 00:11:27 +0530

    Adding New Product To The Cart.

--- app.js
+++ app.js
@@ -1,68 +1,68 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-    .then(user => {
-        req.user = user;
-        next();
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-//.sync({force: true})
-.sync()
-.then(result => {
-    User.findByPk(1);
+  //.sync({force: true})
+  .sync()
+  .then((result) => {
+    return User.findByPk(1);
     //console.log(result);
-})
-.then(user => {
-    if(!user) {
-        User.create({ name:'Test', email: 'test@test.com'});
+  })
+  .then((user) => {
+    if (!user) {
+      User.create({ name: "Test", email: "test@test.com" });
     }
     return user;
-})
-.then(user => {
+  })
+  .then((user) => {
     //console.log(user);
-    return user.createCart(); 
-})
-.then(cart => {
-    app.listen(3000);
-})
-.catch(
-    err => {console.log(err);
-});
 
+    return user.createCart();
+  })
+  .then((cart) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
